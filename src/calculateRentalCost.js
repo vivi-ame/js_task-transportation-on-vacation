58267f9663ad92502e322211fa83fc91ec83bb86
Viktoriia Vasylkivska <viktoriia.vasylkivska@gmail.com>
2026-02-12 13:32:48 +0200

    Changes after reading checklist

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,16 +4,22 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  const oneDayPrice = 40;
-  const finalCost = oneDayPrice * days;
+  const ONE_DAY_PRICE = 40;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
+  let finalCost = ONE_DAY_PRICE * days;
 
-  if (days >= 7) {
-    return finalCost - 50;
-  } else if (days >= 3) {
-    return finalCost - 20;
+  if (days >= LONG_TERM) {
+    return finalCost - LONG_TERM_DISCOUNT;
+  }
+  else if (days >= SHORT_TERM) {
+    return finalCost - SHORT_TERM_DISCOUNT;
   }
 
   return finalCost;
+
 }
 
 module.exports = calculateRentalCost;
